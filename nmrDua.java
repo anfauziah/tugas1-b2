c8096b60bb88097cdd29cf686b87bb601c812471
anfauziah <61958967+anfauziah@users.noreply.github.com>
2020-03-10 19:25:35 +0800

    Add files via upload

---
+++ nmrDua.java
@@ -0,0 +1,6 @@
+public class nmrDua{
+	public static void main(String[] args) {
+
+	System.out.printf("Belajar bahasa pemrograman java");
+	}
+}
---
+++ nmrSatu.java
@@ -0,0 +1,6 @@
+public class nmrSatu{
+	public static void main(String[] args) {
+
+	System.out.printf("selamat belajar");
+	}
+}
